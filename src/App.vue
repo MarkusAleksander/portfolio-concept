17d0bb42691215a3ecf9c163ff96540471e5b213
MarkusAleksander <markholden1989@googlemail.com>
2020-06-23 17:52:22 +0100

    major updates

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app" class="app--black-white">
+    <div id="app" class="app--black-white" ref="app">
         <router-view />
         <toolbar-component />
     </div>
@@ -10,6 +10,57 @@ import ToolbarComponent from "./components/ToolbarComponent/ToolbarComponent";
 export default {
     components: {
         ToolbarComponent,
+    },
+    data: function() {
+        return {
+            is_scrolling: false,
+        };
+    },
+    mounted: function() {
+        // * mock a timeout
+        window.setTimeout(() => {
+            window.hideLoadingOverlay(this.onLoaded);
+        }, 0);
+        // * setup scroll events
+        let app = this.$refs.app;
+        app.addEventListener("wheel", (e) => {
+            if (this.is_scrolling) return;
+            this.is_scrolling = true;
+            this.handleScroll(e, "wheel");
+        });
+        window.addEventListener("keydown", (e) => {
+            if (this.is_scrolling) return;
+            this.is_scrolling = true;
+            this.handleScroll(e, "key");
+        });
+    },
+    methods: {
+        onLoaded: function() {
+            let app = this.$refs.app;
+            app.classList.add("is-loading");
+            window.setTimeout(() => {
+                app.classList.add("is-loaded");
+                app.classList.remove("is-loading");
+            }, 1200);
+        },
+        handleScroll: function(evt, evtType) {
+            console.log(evt);
+            let direction;
+            // -1 == upwards, 1 == downwards
+            if (evtType === "wheel") {
+                direction = evt.deltaY > 0 ? 1 : -1;
+            } else if (evtType === "key") {
+                direction =
+                    evt.code == "ArrowDown"
+                        ? 1
+                        : evt.code == "ArrowUp"
+                        ? -1
+                        : 0;
+            } else if (evtType === "drag") {
+                // ...
+            }
+            console.log(direction);
+        },
     },
 };
 </script>
--- src/containers/HomeContainer.vue
+++ src/containers/HomeContainer.vue
@@ -2,16 +2,43 @@
     <div class="container container--home">
         <div class="block blend--difference">
             <h1>
-                Hi!
-                <br />I'm Markus
-                <br />Alexander Holden.
+                <span>
+                    Hi!
+                    <br />I'm Markus
+                    <br />Alexander Holden.
+                </span>
             </h1>
             <p>
-                I'm a Manchester based Frontend Developer. Focused on writing
-                elegant code and making beautiful and responsive websites.
+                <span>
+                    I'm a Manchester based Frontend Developer. Focused on writing
+                    elegant code and making beautiful and responsive websites.
+                </span>
             </p>
             <div class="navigator-hint">
-                <div class="navigator-hint--arrow-down"></div>
+                <svg
+                    version="1.1"
+                    baseProfile="full"
+                    width="50"
+                    height="50"
+                    viewBox="0 0 80 80"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle
+                        cx="40"
+                        cy="40"
+                        r="38"
+                        stroke="#fff"
+                        fill="transparent"
+                        stroke-width="1"
+                    />
+                    <line x1="40" x2="40" y1="20" y2="60" stroke="#fff" stroke-width="2" />
+                    <polyline
+                        points="28 48 40 60 52 48"
+                        stroke="#fff"
+                        fill="transparent"
+                        stroke-width="2"
+                    />
+                </svg>
             </div>
         </div>
     </div>
