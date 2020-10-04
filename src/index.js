6fe2f76a7ff3c8028565a0fb76970576aef2fc55
MarkusAleksander <markholden1989@googlemail.com>
2020-10-04 21:18:32 +0100

    major design updates

--- src/index.js
+++ src/index.js
@@ -16,3 +16,41 @@ for (let i = 0; i < nav_items.length; i++) {
 
     nav_item.addEventListener("click", doScrolling.bind(null, target, 1500));
 }
+
+let nav_menu_items = document.querySelectorAll(".navigation__link");
+let sections = [];
+let current_idx = null;
+let current_target = null;
+
+for (let i = 0; i < nav_menu_items.length; i++) {
+    let nav_menu_item = nav_menu_items[i];
+    let target = nav_menu_item.getAttribute("data-target");
+
+    if (target) {
+        sections.push(document.querySelector(target));
+    }
+}
+
+window.addEventListener("scroll", function () {
+    let target_idx = 0;
+
+    for (let i = 0; i < sections.length; i++) {
+        let top = sections[i].getBoundingClientRect().top;
+        if (top <= 5) {
+            target_idx = i;
+        }
+    }
+
+    console.log(target_idx);
+
+    if (current_idx === target_idx) return;
+
+    if (current_target) {
+        current_target.classList.remove("active");
+    }
+
+    current_target = nav_menu_items[target_idx];
+
+    current_target.classList.add("active");
+
+});
